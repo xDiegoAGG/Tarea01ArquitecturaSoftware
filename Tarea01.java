a1028601dc436079db09e0f98fef1cfce4573ec2
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-07-24 10:55:15 -0500

    Update Tarea01.java

--- Tarea01.java
+++ Tarea01.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-class Main {
+class Tarea01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         File archivo = new File("Tutorial01.txt");
