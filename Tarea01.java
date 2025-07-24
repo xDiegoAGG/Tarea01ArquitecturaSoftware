8a9214c1c8d9c3f411ae25f2f762cf3e1e57ad3c
Diego Andres Gonzalez Graciano <dagonzal11@eafit.edu.co>
2025-07-23 19:39:58 -0500

    Tarea 01 Update

--- Tarea01.java
+++ Tarea01.java
@@ -1,5 +1,27 @@
-class Main{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        File archivo = new File("Tutorial01.txt");
+        PrintWriter output = null;
+        
+        System.out.print("Hola, Bienvenido a nuestro Tutorial 01 - Arquitectura de Software\nPor favor ingresa tu nombre: ");
+        String nombre = scan.nextLine();
+
+        try {
+            output = new PrintWriter(archivo);
+            output.println(nombre);
+            System.out.println("Se ha guardado correctamente el nombre " + nombre + " en el archivo .txt");
+        } catch (FileNotFoundException e){
+            System.out.println("Ha ocurrido un error y no hemos podido guardar tu nombre.");
+        } finally {
+            if (output != null){
+                output.close();
+            }
+        }
     }
 }
