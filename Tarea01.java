a7b18c702b2c4cef88be57a53562a8cf360fc99d
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-07-24 13:56:57 -0500

    Final Final commit
    Code Done

--- Tarea01.java
+++ Tarea01.java
@@ -7,20 +7,20 @@ class Tarea01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         File archivo = new File("Tutorial01.txt");
-        PrintWriter output = null;
+        PrintWriter writer = null;
         
         System.out.print("Hola, Bienvenido a nuestro Tutorial 01 - Arquitectura de Software\nPor favor ingresa tu nombre: ");
         String nombre = scan.nextLine();
 
         try {
-            output = new PrintWriter(archivo);
-            output.println(nombre);
+            writer = new PrintWriter(archivo);
+            writer.println(nombre);
             System.out.println("Se ha guardado correctamente el nombre " + nombre + " en el archivo .txt");
         } catch (FileNotFoundException e){
             System.out.println("Ha ocurrido un error y no hemos podido guardar tu nombre.");
         } finally {
-            if (output != null){
-                output.close();
+            if (writer != null){
+                writer.close();
             }
         }
     }
